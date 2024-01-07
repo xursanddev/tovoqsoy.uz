fd8c17053069fc338a649b2491dacdc4b61fb5e0
Dilshod Fayzullayev <brogrammer.devs@gmail.com>
2024-01-07 21:34:42 +0500

    :sparkles: A waiting window has been created for the page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App.jsx";
+import "halfmoon/css/halfmoon.min.css";
+import "@fontsource/comfortaa/400.css";
+import "halfmoon/css/cores/halfmoon.modern.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+createRoot(document.getElementById("root"))
+    .render(<StrictMode>
+        <App/>
+    </StrictMode>);
