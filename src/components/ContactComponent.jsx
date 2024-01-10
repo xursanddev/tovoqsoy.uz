2602c7f78640dd66a97c301f32487a0c1ee0cd25
Dilshod Fayzullayev <brogrammer.devs@gmail.com>
2024-01-10 21:33:44 +0500

    :sparkles: Added a connection via Telegram to transfer contact information

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import {Col, Container, Form, Row} from "reactstrap";
 import {useTranslation} from "react-i18next";
 import {YMaps, Map, Clusterer, Placemark} from "@pbe/react-yandex-maps";
+import {sendMessage} from "../utils/util.js";
 
 const ContactComponent = () => {
     const {t} = useTranslation();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        let t = e.target;
+        const [name, phone, desc] = [t[0], t[1], t[2]];
+        sendMessage(name.value, phone.value, desc.value);
+    };
 
     return (
         <div className={"contact"}>
@@ -34,23 +42,23 @@ const ContactComponent = () => {
                         </YMaps>;
                     </Col>
                     <Col>
-                        <Form className={"form text-start"}>
+                        <Form onSubmit={onSubmit} className={"form text-start"}>
                             <div className={"mb-3"}>
                                 <label className={"form-label w-100"}>
                                     {t("contact.firstname")} <br/>
-                                    <input className={"mt-1"} type="text"/>
+                                    <input className={"mt-1"} type="text" required/>
                                 </label>
                             </div>
                             <div className={"mb-3"}>
                                 <label className={"form-label w-100"}>
                                     {t("contact.phone")}<br/>
-                                    <input className={"mt-1"} type="text"/>
+                                    <input className={"mt-1"} type="text" required/>
                                 </label>
                             </div>
                             <div className={"mb-3"}>
                                 <label className={"form-label w-100"}>
                                     {t("contact.desc")}<br/>
-                                    <textarea className={"mt-1"} rows={3}></textarea>
+                                    <textarea className={"mt-1"} rows={3} required></textarea>
                                 </label>
                             </div>
                             <div className={"text-center submit"}>
