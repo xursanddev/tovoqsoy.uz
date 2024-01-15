db5aa68ebc062ee9364ed36a876fc7b240ad163d
Dilshod Fayzullayev <brogrammer.devs@gmail.com>
2024-01-15 21:50:13 +0500

    xexe 😎

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,7 @@
 import {init} from "@dreamcoder-uzbek/telegramjs";
 
 export function sendMessage(n, t, d) {
-    const tg = new init("6899102949:AAHNElkwiUXH-Nau3Ok5z8G5DiXF3rQ9Dzw", -1002131950784, "HTML");
-    tg.sendMessage(`<b>Yangi xabar keldi: \n\n👤 ISM:</b> \t<code>${n}</code>\n\t<b>📞 Telefon: </b><code>${t}</code>\n\t<b>💬Kommentariya: </b><i>${d}</i>`);
-    // tg.getUpdates().then(r => console.log(r));
+    const tg = new init("6544530819:AAF2vfGfWfp9u501ER7Z4W1Ri36ugPZk6Jw", -1002068766009, "HTML");
+    // tg.sendMessage(`<b>Yangi xabar keldi: \n\n👤 ISM:</b> \t<code>${n}</code>\n\t<b>📞 Telefon: </b><code>${t}</code>\n\t<b>💬Kommentariya: </b><i>${d}</i>`);
+    tg.getUpdates().then(r => console.log(r));
 }
